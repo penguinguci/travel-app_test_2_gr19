0d96a10b9971f7b30305d9501433477aa2e50e0a
Trần Vũ <vutkd23405@gmail.com>
2025-02-24 00:02:42 +0700

    init project travel-app test 2

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>© 2021 Travel App</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Learn more about us!</p>
+        </div>
+    )
+}
+
+export default About;
