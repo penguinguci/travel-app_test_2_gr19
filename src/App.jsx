2e969fa168326819e2d9f09504acde18116a43b6
Trần Vũ <vutkd23405@gmail.com>
2025-02-25 21:04:49 +0700

    import bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className='bg bg-primary'>Hello</h1>
     </>
   )
 }
