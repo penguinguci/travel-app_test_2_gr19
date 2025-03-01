d6d6c1efd38825244fb474c6aa899b35b040136a
NguyenVuuu <nguyenvu2112004@gmail.com>
2025-03-01 12:54:02 +0700

    form password

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,141 +1,185 @@
-import { useState } from 'react';
-import logoChefify from '../assets/images/chefify.png'
-import avatar from '../assets/images/avatar.png'
-import check from '../assets/images/check.png'
-import './Component.scss'
-import { Link } from 'react-router-dom';
-import AdminLogin from '../pages/AdminLogin';
+import { useState } from "react";
+import logoChefify from "../assets/images/chefify.png";
+import avatar from "../assets/images/avatar.png";
+import check from "../assets/images/check.png";
+import "./Component.scss";
+import { Link } from "react-router-dom";
+import AdminLogin from "../pages/AdminLogin";
 
 const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(
-        localStorage.getItem("user") === "true"
-    );
-    const [showAdminLogin, setShowAdminLogin] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("user") === "true"
+  );
+  const [showAdminLogin, setShowAdminLogin] = useState(false);
 
-    const handleLogout = () => {
-        localStorage.removeItem("user");
-        window.location.reload();
-    };
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    window.location.reload();
+  };
 
-    return (
-        <>
-            <header className='bg-light shadow-sm'> 
-            <div className="container py-3 d-flex align-items-center justify-content-between">
-                <a href="/" className='navbar-brand'>
-                    <img src={logoChefify} alt="logo" className='img-fluid'/>
+  return (
+    <>
+      <header className="bg-light shadow-sm">
+        <div className="container py-3 d-flex align-items-center justify-content-between">
+          <a href="/" className="navbar-brand">
+            <img src={logoChefify} alt="logo" className="img-fluid" />
+          </a>
+
+          <div className="input-group w-25 align-items-center">
+            <span className="input-group-text bg-white border-rounded-end-0">
+              <i className="bi bi-search"></i>
+            </span>
+            <input
+              type="text"
+              className="form-control border-start-0"
+              placeholder="What would you like to cook?"
+            />
+          </div>
+
+          <nav className="d-none d-lg-flex">
+            <ul className="nav">
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-hover link-underline-light">
+                  What to cook
                 </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-hover link-underline-light">
+                  Recipes
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link nav-hover link-underline-light">
+                  Ingredients
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="/contact"
+                  className="nav-link nav-hover link-underline-light"
+                >
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="/about"
+                  className="nav-link nav-hover link-underline-light"
+                >
+                  About Us
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-                <div className="input-group w-25 align-items-center">
-                    <span className='input-group-text bg-white border-rounded-end-0'>
-                        <i className='bi bi-search'></i>
-                    </span>
-                    <input 
-                        type="text"
-                        className='form-control border-start-0'
-                        placeholder='What would you like to cook?' 
-                    /> 
+          <div className="d-flex">
+            {isLoggedIn ? (
+              <>
+                <Link
+                  to="/your-recipe-box"
+                  className="btn btn-outline-pink me-3 d-flex align-items-center"
+                >
+                  <span>
+                    <img className=" img-thumbnail me-1" src={check} alt="" />
+                  </span>
+                  Your Recipe Box
+                </Link>
+                <div className="user-icon pointer-event">
+                  <img src={avatar} alt="" data-bs-toggle="dropdown" />
+                  <ul className="dropdown-menu p-1">
+                    <li>
+                      <button
+                        className="btn btn-pink dropdown-item"
+                        onClick={handleLogout}
+                      >
+                        Logout
+                      </button>
+                    </li>
+                  </ul>
                 </div>
+              </>
+            ) : (
+              <>
+                <button
+                  className="btn btn-outline-pink me-3"
+                  onClick={() => setShowAdminLogin(true)}
+                >
+                  Login
+                </button>
+                <button className="btn btn-pink">Subscribe</button>
+              </>
+            )}
+          </div>
+        </div>
 
-                <nav className='d-none d-lg-flex'>
-                    <ul className="nav">
-                        <li className="nav-item">
-                            <a href="#" className="nav-link nav-hover link-underline-light">What to cook</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link nav-hover link-underline-light">Recipes</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link nav-hover link-underline-light">Ingredients</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/contact" className="nav-link nav-hover link-underline-light">Contact</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href='/about' className="nav-link nav-hover link-underline-light">About Us</a>
-                        </li>
-                    </ul>
-                </nav>
-
-                <div className="d-flex">
-                    {isLoggedIn ? (
-                    <>
-                        <Link to="/your-recipe-box" className="btn btn-outline-pink me-3 d-flex align-items-center">
-                        <span>
-                            <img className=" img-thumbnail me-1" src={check} alt="" />
-                        </span>
-                        Your Recipe Box
-                        </Link>
-                        <div className="user-icon pointer-event">
-                        <img src={avatar} alt="" data-bs-toggle="dropdown" />
-                        <ul className="dropdown-menu p-1">
-                            <li>
-                            <button
-                                className="btn btn-pink dropdown-item"
-                                onClick={handleLogout}
-                            >
-                                Logout
-                            </button>
-                            </li>
-                        </ul>
-                        </div>
-                    </>
-                    ) : (
-                    <>
-                        <button
-                        className="btn btn-outline-pink me-3"
-                        onClick={() => setShowAdminLogin(true)}
-                        >
-                        Login
-                        </button>
-                        <button className="btn btn-pink">Subscribe</button>
-                    </>
-                    )}
-                </div>
+        {/* mobile responsive navbar */}
+        <div className="d-lg-none bg-white border-top">
+          <div className="container py-2 d-flex justify-content-between align-items-center">
+            <button
+              className="btn btn-light"
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              <i className={menuOpen ? "bi bi-x" : "bi bi-list"}></i>
+            </button>
+            <button className="btn btn-light">
+              <i className="bi bi-search"></i>
+            </button>
+          </div>
+          {menuOpen && (
+            <div className="container py-2">
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <a
+                    href="#"
+                    className="nav-link nav-hover link-underline-light"
+                  >
+                    What to cook
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    href="#"
+                    className="nav-link nav-hover link-underline-light"
+                  >
+                    Recipes
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    href="#"
+                    className="nav-link nav-hover link-underline-light"
+                  >
+                    Ingredients
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    href="/contact"
+                    className="nav-link nav-hover link-underline-light"
+                  >
+                    Contact
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    to={"/about"}
+                    className="nav-link nav-hover link-underline-light"
+                  >
+                    About Us
+                  </Link>
+                </li>
+              </ul>
             </div>
-
-            {/* mobile responsive navbar */}
-            <div className="d-lg-none bg-white border-top">
-                <div className="container py-2 d-flex justify-content-between align-items-center">
-                    <button 
-                        className="btn btn-light"
-                        onClick={() => setMenuOpen(!menuOpen)}
-                    >
-                        <i className={menuOpen ? "bi bi-x" : "bi bi-list"}></i>
-                    </button>
-                    <button className="btn btn-light">
-                        <i className="bi bi-search"></i>
-                    </button>
-                </div>
-                {menuOpen && (
-                    <div className="container py-2">
-                        <ul className="nav flex-column">
-                            <li className="nav-item">
-                                <a href="#" className="nav-link nav-hover link-underline-light">What to cook</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="#" className="nav-link nav-hover link-underline-light">Recipes</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="#" className="nav-link nav-hover link-underline-light">Ingredients</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="/contact" className="nav-link nav-hover link-underline-light">Contact</a>
-                            </li>
-                            <li className="nav-item">
-                                <Link to={"/about"} className="nav-link nav-hover link-underline-light">About Us</Link>
-                            </li>
-                        </ul>
-                    </div>
-                )}
-            </div>
-        </header>
-        <AdminLogin
-            show={showAdminLogin}
-            handleClose={() => setShowAdminLogin(false)}
-        />
+          )}
+        </div>
+      </header>
+      <AdminLogin
+        show={showAdminLogin}
+        handleClose={() => setShowAdminLogin(false)}
+      />
     </>
-    );
-}
+  );
+};
 
 export default Header;
--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -3,105 +3,110 @@ import egg from "../assets/images/egg.jpg";
 import { useState } from "react";
 
 const AdminLogin = ({ show, handleClose }) => {
-    const [error, setError] = useState("");
-    const [email, setEmail] = useState("");
-    if (!show) {
-      return null;
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  if (!show) {
+    return null;
+  }
+
+  const validEmail = (email) => {
+    const re = /^\w+@(gmail\.com|yahoo\.com)$/;
+    return re.test(email);
+  };
+
+  const handleLogin = () => {
+    if (!validEmail(email)) {
+      setError("*Please enter a valid email address.");
+      return;
     }
-
-    const validEmail = (email) => {
-      const re = /^\w+@(gmail\.com|yahoo\.com)$/;
-      return re.test(email);
-    };
-
-    const handleLogin = () => {
-      if (!validEmail(email)) {
-        setError("*Please enter a valid email address.");
-        return;
-      }
-      localStorage.setItem("user", "true");
-      handleClose();
-      window.location.reload();
-    };
-    return (
-      <>
+    localStorage.setItem("user", "true");
+    handleClose();
+    window.location.reload();
+  };
+  return (
+    <>
+      <div
+        className="modal fade show"
+        style={{ display: "block" }}
+        tabIndex="-1"
+        role="dialog"
+      >
         <div
-          className="modal fade show"
-          style={{ display: "block" }}
-          tabIndex="-1"
-          role="dialog"
+          className="modal-dialog modal-dialog-centered modal-lg"
+          role="document"
         >
-          <div
-            className="modal-dialog modal-dialog-centered modal-lg"
-            role="document"
-          >
-            <div className="mdal-content modal-content">
-              <div className="modal-body d-flex p-0">
-                {/* img (left) */}
-                <div className="modal-left d-none d-md-block">
-                  <img src={egg} alt="Food" className="img-fluid" />
-                  <p className="overlay-text">
-                    "Embrace the art of cooking, where flavors come alive!"
-                  </p>
+          <div className="mdal-content modal-content">
+            <div className="modal-body d-flex p-0">
+              {/* img (left) */}
+              <div className="modal-left d-none d-md-block">
+                <img src={egg} alt="Food" className="img-fluid" />
+                <p className="overlay-text">
+                  "Embrace the art of cooking, where flavors come alive!"
+                </p>
+              </div>
+
+              {/* form login (right) */}
+              <div className="modal-right p-4">
+                <button
+                  type="button"
+                  className="btn-cls close-btn"
+                  onClick={handleClose}
+                >
+                  &times;
+                </button>
+                <h4 className="login-text">Login</h4>
+                <p className="text-mail">Enter your email to log in.</p>
+                <input
+                  type="email"
+                  className="input-mail form-control mb-3"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                    setError("");
+                  }}
+                />
+                <input
+                  type="password"
+                  className="input-mail form-control mb-3"
+                  placeholder="Password"
+                />
+                {error && <small className="text-danger">{error}</small>}
+                <button
+                  className="btn btn-pink btn-block"
+                  onClick={handleLogin}
+                >
+                  Continue
+                </button>
+
+                <div className="text-or text-center my-3 d-flex text-center align-items-center">
+                  <hr />
+                  <span className="fw-bold">OR</span>
+                  <hr />
                 </div>
+                <button className="btn btn-outline-pink me-3">Sign Up</button>
+                <p className="srv text-center ">
+                  By continuing, you agree to the updated <b>Terms of Sale</b>,{" "}
+                  <b>Terms of Service</b>, and <b>Privacy Policy</b>.
+                </p>
 
-                {/* form login (right) */}
-                <div className="modal-right p-4">
-                  <button
-                    type="button"
-                    className="btn-cls close-btn"
-                    onClick={handleClose}
-                  >
-                    &times;
-                  </button>
-                  <h4 className="login-text">Login</h4>
-                  <p className="text-mail">Enter your email to log in.</p>
-                  <input
-                    type="email"
-                    className="input-mail form-control mb-3"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => {
-                      setEmail(e.target.value);
-                      setError("");
-                    }}
-                  />
-                  {error && <small className="text-danger">{error}</small>}
-                  <button
-                    className="btn btn-pink btn-block"
-                    onClick={handleLogin}
-                  >
-                    Continue
-                  </button>
-
-                  <div className="text-or text-center my-3 d-flex text-center align-items-center">
-                    <hr className="" />
-                    <span className="fw-bold">OR</span>
-                    <hr />
-                  </div>
-
-                  <p className="srv text-center ">
-                    By continuing, you agree to the updated <b>Terms of Sale</b>,{" "}
-                    <b>Terms of Service</b>, and <b>Privacy Policy</b>.
-                  </p>
-
-                  <button className="btn-google ">
-                    <i className=" bi bi-google me-2"></i> Continue with Google
-                  </button>
-                  <button className="btn-fb">
-                    <i className="bi bi-facebook me-2"></i> Continue with Facebook
-                  </button>
-                  <button className="apple-fb">
-                    <i className="bi bi-apple me-2"></i> Continue with Apple
-                  </button>
-                </div>
+                <button className="btn-google ">
+                  <i className=" bi bi-google me-2"></i> Continue with Google
+                </button>
+                <button className="btn-fb">
+                  <i className="bi bi-facebook me-2"></i> Continue with Facebook
+                </button>
+                <button className="apple-fb">
+                  <i className="bi bi-apple me-2"></i> Continue with Apple
+                </button>
               </div>
             </div>
           </div>
         </div>
-        <div className="modal-backdrop fade show"></div>
-      </>
-    );
+      </div>
+      <div className="modal-backdrop fade show"></div>
+    </>
+  );
 };
 
 export default AdminLogin;
